e114b4ff2b050b5cbaaeed54b14155ad2f7cbef0
Ryan <dragonguard7@gmail.com>
2017-03-25 23:15:20 -0400

    Make all portfolio pictures allow pop up view and added button to redirct to either the live preview or the source code from github.

--- js/controllers/PortfolioController.js
+++ js/controllers/PortfolioController.js
@@ -12,13 +12,14 @@ app.controller('portfolioController', function($scope){
 	   
    };
      
-   $scope.types = ["All", "java", "angular", "C", "C++"];
+
    $scope.images = [ 
 	  { 
 	    icon: 'img/snake.jpg', 
 	    title: 'Snake', 
 	    description: 'Slither around, eat and grow!',
 		type: "Angular",
+		link: true,
 		url: "portfolio_projects/snake/index.html"
 	  }, 
 	  { 
@@ -26,6 +27,7 @@ app.controller('portfolioController', function($scope){
 	    title: 'Carousel', 
 	    description: 'Look through pictures in style',
 		type: "Angular",
+		link: true,
 		url: "portfolio_projects/Carousel/index.html"
 	  },
 	  {
@@ -33,6 +35,7 @@ app.controller('portfolioController', function($scope){
 	    title: 'Senior Design Project',
 	    description: 'Self flying, tracking airplane',
 		type: "C",
+		link: true,
 		url: "http://www.eecs.ucf.edu/seniordesign/fa2012sp2013/g02/"
 	  },
 	  {
@@ -40,49 +43,56 @@ app.controller('portfolioController', function($scope){
 	    title: 'Breakout',
 	    description: 'Destroy all the bricks',
 		type: "Java",
-		url: "No link"
+		link: false,
+		url: "https://github.com/dragonguard7/JavaProjects"
 	  }, 
 	  { 
 	    icon: 'img/hitori.jpg', 
 	    title: 'Hitori Solver', 
 	    description: 'Give it a puzzle to solve',
 		type: "Java",
-		url: "No link"
+		link: false,
+		url: "https://github.com/dragonguard7/JavaProjects"
 	  },
 	  {
 	    icon: 'img/linkList.jpg',
 	    title: 'Linked list',
 	    description: 'Adjust the list to your desire',
 		type: "C++",
-		url: "No link"
+		link: false,
+		url: "https://github.com/dragonguard7/linked-list-manipulation"
 	  },
 	  {
 		icon: 'img/mazeRunner.jpg',
 		title: 'Maze Runner',
 		description: 'Look for the goal!',
 		type: "Java",
-		url: "No link"
+		link: false,
+		url: "https://github.com/dragonguard7/JavaProjects"
 	  },
 	  {
 	    icon: 'img/RPG.jpg',
 	    title: 'Tile RPG',
 	    description: 'Explore the world!',
 		type: "Java",
-		url: "No link"
+		link: false,
+		url: "https://github.com/dragonguard7/rpg"
 	  },
 	  {
 	    icon: 'img/reader.jpg',
 	    title: 'Reader',
 	    description: 'Navigate through books',
 		type: "Angular",
+		link: true,
 		url: "portfolio_projects/Reader/index.html"
 	  },
 	  { 
-		    icon: 'img/sudoku.jpg', 
-		    title: 'Sudoku Solver', 
-		    description: 'Give it a puzzle to solve',
-			type: "Java",
-			url: "No link"
+		icon: 'img/sudoku.jpg', 
+		title: 'Sudoku Solver', 
+		description: 'Give it a puzzle to solve',
+		type: "Java",
+		link: false,
+		url: "https://github.com/dragonguard7/JavaProjects"
 	  }
 	];
 
